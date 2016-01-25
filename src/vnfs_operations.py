8db3f34fce5b0de828cdd76362463698b2907536
Shihabur Rahman Chowdhury <sr2chowdhury@uwaterloo.ca>
2016-01-24 23:34:59 -0500

    Add source code documentation and fix formatting.

--- src/vnfs_operations.py
+++ src/vnfs_operations.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 from __future__ import with_statement
 
 import os
@@ -15,9 +14,14 @@ from hypervisor import hypervisor_return_codes as hrc
 
 
 class VNFSOperations:
+
+    """
+    Provides a common set of operations for nfio. These operations act as a
+    helper.
+    """
+
     OP_UNDEFINED = 0xFF
-    OP_NF = 0o01
-    OP_CHAIN = 101
+    OP_NF = 0x01
 
     def __init__(self, vnfs_root):
         self.vnfs_root = vnfs_root
@@ -30,6 +34,17 @@ class VNFSOperations:
         return path
 
     def vnfs_create_vnf_instance(self, path, mode):
+        """
+        Create the file system structure for a VNF.
+
+        Args:
+            path: path of the new VNF instance.
+            mode: file creation mode for the new VNF instance directory.
+
+        Returns:
+            returns the return code of os.mkdir
+        """
+
         full_path = self._full_path(path)
         result = os.mkdir(full_path)
         default_file_mode = 0o644
@@ -66,22 +81,35 @@ class VNFSOperations:
         return result
 
     def vnfs_get_opcode(self, path):
+        """
+        Determinse the type of operation based on the path.
+
+        Args:
+            path: path to the file/directory on which the operation is being
+            performed
+
+        Returns:
+            If the file is under nf-types subdirectory in the nfio mount, then
+            returns OP_NF. Otherwise, returns OP_UNDEFINED.
+        """
         tokens = self._full_path(path).encode('ascii').split('/')
         if "nf-types" in tokens:
             return VNFSOperations.OP_NF
-        elif "chns" in tokens:
-            return VNFSOperations.OP_CHAIN
         return VNFSOperations.OP_UNDEFINED
 
-    def vnfs_create_chain(self, path, mode):
-        full_path = self._full_path(path)
-        result = 0
-        result = os.mkdir(full_path)
-        os.open(full_path + "/status", os.O_WRONLY | os.O_CREAT, mode)
-        os.open(full_path + "/action", os.O_WRONLY | os.O_CREAT, mode)
-        return result
-
     def vnfs_get_nf_type(self, path):
+        """
+        Parse the type of VNF from path
+
+        Args:
+            path: the path of the file/directory on which some operation is
+            being performed.
+
+        Returns:
+            Returns the type of VNF parsed from the path, e.g., if the path is
+            /mnt/vnfsroot/nf-types/firewall/fw-alpha/action then returns
+            firewall.
+        """
         tokens = self._full_path(path).encode('ascii').split('/')
         try:
             return tokens[tokens.index("nf-types") + 1]
@@ -90,40 +118,57 @@ class VNFSOperations:
         except IndexError:
             return ""
 
-    def vnfs_get_chain_name(self, path):
-        tokens = self._full_path(path).encode('ascii').split('/')
-        try:
-            return tokens[tokens.index("chns") + 1]
-        except ValueError:
-            return ""
-        except IndexError:
-            return ""
-
     def vnfs_get_file_name(self, path):
+        """
+        Return the name of the file represented by a path.
+
+        Args:
+            path: the path of the file in concern
+
+        Returns:
+            returns the name of the file, i.e., last token after / in the path.
+        """
+
         return path.split('/')[-1]
 
     def vnfs_is_nf_instance(self, path):
+        """
+        Determines if a path represents an nf instance directory.
+
+        Args:
+            path: path of the file/directory in concern.
+
+        Returns:
+            True: if path represents an nf instance directory. For example, if
+            path is /mnt/vnfsmnt/nf-types/firewall/fw-alpha then returns True.
+
+            False: if the path does not represent an nf instance directory. For
+            example, if path is /mnt/vnfsmnt/nf-types/firewall/fw-alpha/action
+            then returns False.
+        """
         tokens = self._full_path(path).encode('ascii').split('/')
         if ('nf-types' in tokens) and len(tokens) > tokens.index('nf-types') + \
                 1:
             return True
         return False
 
-    def vnfs_get_chain_elements(self, chain_root):
-        return [directory[0] for directory in
-                os.walk(chain_root)
-                if directory[0] != chain_root]
-
-    def vnfs_deploy_nf_chain(self, chain_root):
-        nf_instances = self.vnfs_get_chain_elements(chain_root)
-        nf_paths = [os.readlink(nf_path + "/" +
-                                self.vnfs_get_file_name(nf_path))
-                    for nf_path in nf_instances]
-        ret_codes = [
-            {nf_path: self.vnfs_deploy_nf(nf_path)} for nf_path in nf_paths]
-        return ret_codes
-
     def vnfs_get_instance_configuration(self, nf_path):
+        """
+        Return the configuration parameters related to a VNF instance.
+
+        Args:
+            nf_path: path of the VNF instance. e.g.,
+            /mnt/vnfsmnt/firewall/fw-alpha
+
+        Returns:
+            A tuple representing the configuration of the VNF instance. The
+            tuple is organized in the following order:
+                nf_instance_name: name of the VNF instance.
+                nf_type: type of the VNF.
+                ip_address: IP address of the machine where this VNF will be
+                    deployed.
+                image_name: name of the VM/container image for that VNF.
+        """
         nf_instance_name = self.vnfs_get_file_name(nf_path)
         nf_type = self.vnfs_get_nf_type(nf_path)
         ip_address = ''
@@ -136,6 +181,16 @@ class VNFSOperations:
         return nf_instance_name, nf_type, ip_address, image_name
 
     def vnfs_deploy_nf(self, nf_path):
+        """
+        Deploys a VNF instance.
+
+        Args:
+            nf_path: path of the VNF instance.
+
+        Returns:
+            return codes are descirbed in hypervisor.hypervisor_return_codes
+            module.
+        """
         nf_instance_name, nf_type, ip_address, image_name = self.vnfs_get_instance_configuration(
             nf_path)
         print "Starting " + nf_instance_name + " of type " + nf_type + " at " + ip_address + " with image " + image_name
@@ -151,7 +206,17 @@ class VNFSOperations:
             return deploy_ret_code
 
     def vnfs_stop_vnf(self, nf_path):
-        nf_instance_name, nf_type, ip_address, image_name= self.vnfs_get_instance_configuration(
+        """
+        Stops a VNF instance.
+
+        Args:
+            nf_path: path of the VNF instance.
+
+        Returns:
+            return codes are described in hypervisor.hypervisor_return_codes
+            module.
+        """
+        nf_instance_name, nf_type, ip_address, image_name = self.vnfs_get_instance_configuration(
             nf_path)
         print "Stopping " + nf_instance_name
         cont_id, ret_code = self._hypervisor.get_id(
@@ -161,6 +226,15 @@ class VNFSOperations:
         return response
 
     def vnfs_get_rx_bytes(self, nf_path):
+        """
+        Reads the number of bytes received by a VNF instance.
+
+        Args:
+            nf_path: path of the VNF instance.
+
+        Returns:
+            returns the number of bytes received by a VNF instance.
+        """
         nf_instance_name, nf_type, ip_address, image_name = self.vnfs_get_instance_configuration(
             nf_path)
         cont_id, ret_code = self._hypervisor.get_id(ip_address,
@@ -174,6 +248,15 @@ class VNFSOperations:
         return response
 
     def vnfs_get_tx_bytes(self, nf_path):
+        """
+        Reads the number of bytes sent by a VNF instance.
+
+        Args:
+            nf_path: path of the VNF instance.
+
+        Returns:
+            returns the number of bytes sent by a VNF instance.
+        """
         nf_instance_name, nf_type, ip_address, image_name = self.vnfs_get_instance_configuration(
             nf_path)
         cont_id, ret_code = self._hypervisor.get_id(ip_address,
@@ -187,6 +270,15 @@ class VNFSOperations:
         return response
 
     def vnfs_get_pkt_drops(self, nf_path):
+        """
+        Reads the number of packets dropped by a VNF instance.
+
+        Args:
+            nf_path: path of the VNF instance.
+
+        Returns:
+            returns the number of packets dropped by a VNF instance.
+        """
         nf_instance_name, nf_type, ip_address, image_name = self.vnfs_get_instance_configuration(
             nf_path)
         cont_id, ret_code = self._hypervisor.get_id(ip_address,
@@ -200,6 +292,18 @@ class VNFSOperations:
         return response
 
     def vnfs_get_status(self, nf_path):
+        """
+        Get the status of a VNF instance, e.g., the VNF is
+        running/suspended/stopped etc.
+
+        Args:
+            nf_path: path of the VNF instance.
+
+        Returns:
+            Hypervisor specific status of the VNF. For example, if Docker is
+            being used for VNF deployment then Docker specific container status
+            message is returned.
+        """
         nf_instance_name, nf_type, ip_address, image_name = self.vnfs_get_instance_configuration(
             nf_path)
         cont_id, ret_code = self._hypervisor.get_id(ip_address,
