c0dbddb7a8a5becae3622d454e45b4de0d99ffb4
faizulbari <faizulbari@gmail.com>
2016-02-16 02:33:22 -0500

    updated doc strings

--- src/hypervisor/docker_driver.py
+++ src/hypervisor/docker_driver.py
@@ -50,7 +50,7 @@ class DockerDriver(HypervisorBase):
             logger.error(ex.message, exc_info=False)
             raise nfioError
 
-    def isEmpty(self, string):
+    def _is_empty(self, string):
         """
         @brief checks whether a string is empty or None
     
@@ -60,16 +60,16 @@ class DockerDriver(HypervisorBase):
         """
         return string is None or string.strip() == ""
 
-    def validate_host(self, host):
-        if self.isEmpty(host):
+    def _validate_host(self, host):
+        if self._is_empty(host):
             raise errors.VNFHostNameIsEmptyError
 
-    def validate_image_name(self, image_name):
-        if self.isEmpty(image_name):
+    def _validate_image_name(self, image_name):
+        if self._is_empty(image_name):
             raise errors.VNFImageNameIsEmptyError
 
-    def validate_cont_name(self, cont_name):
-        if self.isEmpty(cont_name):
+    def _validate_cont_name(self, cont_name):
+        if self._is_empty(cont_name):
             raise errors.VNFNameIsEmptyError
         ## check to see if there is any container with name cont_name
         #nameExists = False
@@ -84,7 +84,8 @@ class DockerDriver(HypervisorBase):
         #    raise errors.VNFNotFoundError
 
     def _get_client(self, host):
-        """Returns a Docker client.
+        """
+        Returns a Docker client.
         
         @param host IP address or hostname of the host (physical/virtual) 
             where docker containers will be deployed
@@ -92,7 +93,7 @@ class DockerDriver(HypervisorBase):
         @return A docker client object that can be used to communicate 
             with the docker daemon on the host
         """
-        self.validate_host(host)
+        self._validate_host(host)
         with self._error_handling(errors.HypervisorConnectionError):
           return docker.Client(
             base_url="http://" +
@@ -101,62 +102,64 @@ class DockerDriver(HypervisorBase):
             self.__port,
             version=self.__version)
 
-    def lookupVNF(self, host, user, vnf_name):
-        self.validate_host(host)
+    def _lookup_vnf(self, host, user, vnf_name):
+        self._validate_host(host)
         vnf_fullname = user + '-' + vnf_name
-        self.validate_cont_name(vnf_fullname)
+        self._validate_cont_name(vnf_fullname)
         dcx = self._get_client(host)
         with self._error_handling(errors.VNFNotFoundError):
             inspect_data = dcx.inspect_container(container=vnf_fullname)
             return dcx, vnf_fullname, inspect_data
 
     def get_id(self, host, user, vnf_name):
-        """Returns a container's ID.
+        """
+        Returns a container's ID.
         
           @param host IP address or hostname of the machine where
             the docker container is deployed
           @param user name of the user who owns the VNF
-          @param vnf_type type of the deployed VNF
           @param vnf_name name of the VNF instance whose ID is being queried
           
           @return docker container ID.
         """
-        dcx, vnf_fullname, inspect_data = self.lookupVNF(host, user, vnf_name)
+        dcx, vnf_fullname, inspect_data = self._lookup_vnf(host, user, vnf_name)
         return inspect_data['Id'].encode('ascii')
 
     def get_ip(self, host, user, vnf_name):
+        """
+        Returns a container's IP address.
+        
+          @param host IP address or hostname of the machine where
+            the docker container is deployed
+          @param user name of the user who owns the VNF
+          @param vnf_name name of the VNF instance whose ID is being queried
+          
+          @return docker container's IP.
+        """
         if self.guest_status(host, user, vnf_name) != 'running':
             raise errors.VNFNotRunningError
-        dcx, vnf_fullname, inspect_data = self.lookupVNF(host, user, vnf_name)
+        dcx, vnf_fullname, inspect_data = self._lookup_vnf(host, user, vnf_name)
         return inspect_data['NetworkSettings']['IPAddress'].encode('ascii')
 
     def deploy(self, host, user, image_name, vnf_name, is_privileged=True):
-        """Deploys a docker container.
-
-        Args:
-            host: IP address or hostname of the machine where
-                the docker container is to be deployed
-            user: name of the user who owns the VNF
-            image_name: docker image name for the VNF
-            vnf_name: name of the VNF instance
-            is_privileged: if True then the container is deployed in 
-                privileged mode
-
-        Returns:
-            If the operation is successful then returns a tuple 
-            consisting of the following values:
-                container_id: docker container id
-                return_code: SUCCESS
-                return_message: EMPTY in this case
-            otherwise returns the error as the following tuple:
-                None as the first value
-                return_code: one of the error codes defined in hypervisor_return_codes
-                return_message: detailed message for the return code
         """
-        self.validate_host(host)
-        self.validate_image_name(image_name)
+        Deploys a docker container.
+
+        
+        @param host IP address or hostname of the machine where 
+            the docker container is to be deployed
+        @param user name of the user who owns the VNF
+        @param image_name docker image name for the VNF
+        @param vnf_name name of the VNF instance
+        @param is_privileged if True then the container is deployed in 
+            privileged mode
+
+        @returns docker container ID
+        """
+        self._validate_host(host)
+        self._validate_image_name(image_name)
         vnf_fullname = user + '-' + vnf_name
-        self.validate_cont_name(vnf_fullname)
+        self._validate_cont_name(vnf_fullname)
         dcx = self._get_client(host)
         host_config = dict()
         if is_privileged:
@@ -170,160 +173,101 @@ class DockerDriver(HypervisorBase):
             return container['Id']
 
     def start(self, host, user, vnf_name, is_privileged=True):
-        """Starts a docker container.
-
-        Args:
-          host: IP address or hostname of the machine where 
-              the docker container is deployed
-          vnf_id: Docker container ID for the VNF
-          is_privileged: if True then the container is started in 
-            privileged mode
-
-        Returns:
-            If the operation is successful then returns a tuple 
-            consisting of the following values:
-                response: response from the docker-py client
-                return_code: SUCCESS
-                return_message: EMPTY in this case
-            otherwise returns the error as the following tuple:
-                None as the first value
-                return_code: one of the error codes defined in hypervisor_return_codes
-                return_message: detailed message for the return code
         """
-        dcx, vnf_fullname, inspect_data = self.lookupVNF(host, user, vnf_name)
+        Starts a docker container.
+
+        @param host IP address or hostname of the machine/VM where 
+              the docker container is deployed
+        @param user name of the user
+        @param vnf_name name of the VNF
+        @param is_privileged if True then the container is started in 
+            privileged mode
+        """
+        dcx, vnf_fullname, inspect_data = self._lookup_vnf(host, user, vnf_name)
         with self._error_handling(errors.VNFStartError):
             dcx.start(container=vnf_fullname,
                 dns=self.__dns_list,
                 privileged=is_privileged)
 
     def restart(self, host, user, vnf_name):
-        """Restarts a docker container.
-
-        Args:
-          host: IP address or hostname of the machine where 
-              the docker container is deployed
-          vnf_id: Docker container ID for the VNF
-
-        Returns:
-            If the operation is successful then returns a tuple 
-            consisting of the following values:
-                response: response from the docker-py client
-                return_code: SUCCESS
-                return_message: EMPTY in this case
-            otherwise returns the error as the following tuple:
-                None as the first value
-                return_code: one of the error codes defined in hypervisor_return_codes
-                return_message: detailed message for the return code
         """
-        dcx, vnf_fullname, inspect_data = self.lookupVNF(host, user, vnf_name)
+        Restarts a docker container.
+
+        @param host IP address or hostname of the machine/VM where 
+              the docker container is deployed
+        @param user name of the user
+        @param vnf_name name of the VNF
+        """
+        dcx, vnf_fullname, inspect_data = self._lookup_vnf(host, user, vnf_name)
         with self._error_handling(errors.VNFRestartError):
             dcx.restart(container=vnf_fullname)
 
     def stop(self, host, user, vnf_name):
-        """Stops a docker container.
-
-        Args:
-          host: IP address or hostname of the machine where 
-              the docker container is deployed
-          vnf_id: Docker container ID for the VNF
-
-        Returns:
-            If the operation is successful then returns a tuple 
-            consisting of the following values:
-                response: response from the docker-py client
-                return_code: SUCCESS
-                return_message: EMPTY in this case
-            otherwise returns the error as the following tuple:
-                None as the first value
-                return_code: one of the error codes defined in hypervisor_return_codes
-                return_message: detailed message for the return code
         """
-        dcx, vnf_fullname, inspect_data = self.lookupVNF(host, user, vnf_name)
+        Stops a docker container.
+
+        @param host IP address or hostname of the machine/VM where 
+              the docker container is deployed
+        @param user name of the user
+        @param vnf_name name of the VNF
+        """
+        dcx, vnf_fullname, inspect_data = self._lookup_vnf(host, user, vnf_name)
         with self._error_handling(errors.VNFStopError):
             dcx.stop(container=vnf_fullname)
 
     def pause(self, host, user, vnf_name):
-        """Pauses a docker container.
-
-        Args:
-          host: IP address or hostname of the machine where 
-              the docker container is deployed
-          vnf_id: Docker container ID for the VNF
-
-        Returns:
-            If the operation is successful then returns a tuple 
-            consisting of the following values:
-                response: response from the docker-py client
-                return_code: SUCCESS
-                return_message: EMPTY in this case
-            otherwise returns the error as the following tuple:
-                None as the first value
-                return_code: one of the error codes defined in hypervisor_return_codes
-                return_message: detailed message for the return code
         """
-        dcx, vnf_fullname, inspect_data = self.lookupVNF(host, user, vnf_name)
+        Pauses a docker container.
+
+        @param host IP address or hostname of the machine/VM where 
+              the docker container is deployed
+        @param user name of the user
+        @param vnf_name name of the VNF
+        """
+        dcx, vnf_fullname, inspect_data = self._lookup_vnf(host, user, vnf_name)
         with self._error_handling(errors.VNFPauseError):
             dcx.pause(container=vnf_fullname)
 
     def unpause(self, host, user, vnf_name):
         """Unpauses a docker container.
 
-        Args:
-          host: IP address or hostname of the machine where 
+        @param host IP address or hostname of the machine/VM where 
               the docker container is deployed
-          vnf_id: Docker container ID for the VNF
-
-        Returns:
-            If the operation is successful then returns a tuple 
-            consisting of the following values:
-                response: response from the docker-py client
-                return_code: SUCCESS
-                return_message: EMPTY in this case
-            otherwise returns the error as the following tuple:
-                None as the first value
-                return_code: one of the error codes defined in hypervisor_return_codes
-                return_message: detailed message for the return code
+        @param user name of the user
+        @param vnf_name name of the VNF
         """
-        dcx, vnf_fullname, inspect_data = self.lookupVNF(host, user, vnf_name)
+        dcx, vnf_fullname, inspect_data = self._lookup_vnf(host, user, vnf_name)
         with self._error_handling(errors.VNFUnpauseError):
             dcx.unpause(container=vnf_fullname)
 
     def destroy(self, host, user, vnf_name, force=True):
-        """Destroys a docker container.
-
-        Args:
-          host: IP address or hostname of the machine where 
-              the docker container is deployed
-          vnf_id: Docker container ID for the VNF
-
-        Returns:
-            If the operation is successful then returns a tuple 
-            consisting of the following values:
-                response: response from the docker-py client
-                return_code: SUCCESS
-                return_message: EMPTY in this case
-            otherwise returns the error as the following tuple:
-                None as the first value
-                return_code: one of the error codes defined in hypervisor_return_codes
-                return_message: detailed message for the return code
         """
-        dcx, vnf_fullname, inspect_data = self.lookupVNF(host, user, vnf_name)
+        Destroys a docker container.
+
+        @param host IP address or hostname of the machine/VM where 
+              the docker container is deployed
+        @param user name of the user
+        @param vnf_name name of the VNF
+        @param force if set to False then a running VNF will not 
+              be destroyed. default is True
+        """
+        dcx, vnf_fullname, inspect_data = self._lookup_vnf(host, user, vnf_name)
         with self._error_handling(errors.VNFDestroyError):
             dcx.remove_container(container=vnf_fullname, force=force)
 
     def execute_in_guest(self, host, user, vnf_name, cmd):
-        """Executed commands inside a docker container.
-
-        Args:
-          host: IP address or hostname of the machine where 
-              the docker container is deployed
-          vnf_id: Docker container ID for the VNF
-          cmd: the command to execute inside the container
-
-        Returns:
-          The output of the command passes as cmd
         """
-        dcx, vnf_fullname, inspect_data = self.lookupVNF(host, user, vnf_name)
+        Executed commands inside a docker container.
+
+        @param host IP address or hostname of the machine/VM where 
+              the docker container is deployed
+        @param user name of the user
+        @param vnf_name name of the VNF
+        @param cmd the command to execute inside the container
+
+        @returns The output of the command passes as cmd
+        """
+        dcx, vnf_fullname, inspect_data = self._lookup_vnf(host, user, vnf_name)
         if self.guest_status(host, user, vnf_name) != 'running':
             raise errors.VNFNotRunningError
         with self._error_handling(errors.VNFCommandExecutionError):
@@ -332,23 +276,15 @@ class DockerDriver(HypervisorBase):
             return response
 
     def guest_status(self, host, user, vnf_name):
-        """Returns the status of a docker container.
-
-        Args:
-          host: IP address or hostname of the machine where 
-              the docker container is deployed
-          vnf_id: Docker container ID for the VNF
-
-        Returns:
-            If the operation is successful then returns a tuple 
-            consisting of the following values:
-                state: current state of the docker container
-                return_code: SUCCESS
-                return_message: EMPTY in this case
-            otherwise returns the error as the following tuple:
-                Undefined as the first value
-                return_code: one of the error codes defined in hypervisor_return_codes
-                return_message: detailed message for the return code
         """
-        dcx, vnf_fullname, inspect_data = self.lookupVNF(host, user, vnf_name)
+        Returns the status of a docker container.
+
+        @param host IP address or hostname of the machine/VM where 
+              the docker container is deployed
+        @param user name of the user
+        @param vnf_name name of the VNF
+
+        @returns current state of the docker container
+        """
+        dcx, vnf_fullname, inspect_data = self._lookup_vnf(host, user, vnf_name)
         return inspect_data['State']['Status'].encode('ascii')
